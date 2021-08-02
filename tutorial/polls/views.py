7cd6189db0291ac2c45a749efee2666c67e48c52
Ryosuke Surface <r-shimada0930@gmail.com>
2021-08-02 21:22:05 +0900

    tutorial(3)3

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from django.utils.html import mark_safe
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Hello, world! You're at the polls index.")
-    return render(request, 'polls/index.html',{})
+    '''return render(request, 'polls/index.html',{
+        'hoge': 'test string',
+        'fuga': '<br>tag<br>',
+        'piyo': mark_safe('<br>tag<br>')
+    })'''
+
+    return render(request, 'polls/index.html', {
+        'questions': Question.objects.all(),
+    })
+
