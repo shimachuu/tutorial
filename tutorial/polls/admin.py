9a7c0a8350c4cca94fc31a43f4fe69bc7e302fdf
Ryosuke Surface <r-shimada0930@gmail.com>
2021-08-14 15:48:02 +0900

    tutorialから脱線して駅名しりとりへ（import_exportの導入）

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -1,19 +1,31 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
 from .models import Question
 from .models import Choice
 
+class QuestionResource(resources.ModelResource):
+    # Modelに対するdjango-import-exportの設定
+    class Meta:
+        model = Question
+
+
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(ImportExportModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('pk','question_text', 'pub_date','was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
+    # django-import-exportsの設定
+    resource_class = QuestionResource
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
